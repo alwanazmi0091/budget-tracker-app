f83c226a196639f05ff3a485a4a84de8c35cf9b4
alwanazmi0091 <alwanazmi0091@gmail.com>
2026-05-18 13:51:36 +0700

    changed the formating

--- script.js
+++ script.js
@@ -20,319 +20,347 @@ const newAmount = document.querySelector("#new-amount");
 const cancelEditBtn = document.querySelector("#cancel-edit");
 const editExpBtn = document.querySelector("#edit-exp-btn");
 
-let monthExpensesData = JSON.parse(localStorage.getItem("month-expenses")) || [];
-const validMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let monthExpensesData =
+  JSON.parse(localStorage.getItem("month-expenses")) || [];
+const validMonths = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 let currentMonthOpenedId = null;
 let currentExpenseEditedId = null;
 
 function saveData() {
-    localStorage.setItem("month-expenses", JSON.stringify(monthExpensesData));
+  localStorage.setItem("month-expenses", JSON.stringify(monthExpensesData));
 }
 
 function renderMonths() {
-    monthList.innerHTML = "";
+  monthList.innerHTML = "";
 
-    monthExpensesData.forEach(month => {
-        const li = document.createElement("li");
-        li.className = "month";
+  monthExpensesData.forEach((month) => {
+    const li = document.createElement("li");
+    li.className = "month";
 
-        const button = document.createElement("button");
-        button.id = month.id;
-        button.textContent = month.month;
+    const button = document.createElement("button");
+    button.id = month.id;
+    button.textContent = month.month;
 
-        button.addEventListener("click", () => {
-            currentMonthOpenedId = month.id;
+    button.addEventListener("click", () => {
+      currentMonthOpenedId = month.id;
 
-            const currActive = document.querySelector(".month.active");
-            if (currActive) {
-                currActive.classList.remove("active");
-            }
-            li.classList.add("active");
+      const currActive = document.querySelector(".month.active");
+      if (currActive) {
+        currActive.classList.remove("active");
+      }
+      li.classList.add("active");
 
-            updateMainPanelStats();
-            renderExpenses();
-            expenseForm.reset();
-        })
+      updateMainPanelStats();
+      renderExpenses();
+      expenseForm.reset();
+    });
 
-        const delBtn = document.createElement("button");
-        delBtn.className = "del-month-btn";
+    const delBtn = document.createElement("button");
+    delBtn.className = "del-month-btn";
 
-        delBtn.addEventListener("click", (e) => {
-            e.stopPropagation();
-            deleteMonth(month.id);
-        })
+    delBtn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      deleteMonth(month.id);
+    });
 
-        const i = document.createElement("i")
-        i.className = "fa-regular fa-calendar-xmark";
+    const i = document.createElement("i");
+    i.className = "fa-regular fa-calendar-xmark";
 
-        delBtn.appendChild(i);
+    delBtn.appendChild(i);
 
-        li.appendChild(delBtn);
-        li.appendChild(button);
-        monthList.appendChild(li);
-    })
+    li.appendChild(delBtn);
+    li.appendChild(button);
+    monthList.appendChild(li);
+  });
 }
 
 function addMonth() {
-    const month = monthInput.value.trim().toLowerCase();
-    if (!month) {
-        monthInput.setCustomValidity("Please enter a month.");
-        monthInput.reportValidity("");
-        return;
-    }
-    monthInput.setCustomValidity("");
+  const month = monthInput.value.trim().toLowerCase();
+  if (!month) {
+    monthInput.setCustomValidity("Please enter a month.");
+    monthInput.reportValidity("");
+    return;
+  }
+  monthInput.setCustomValidity("");
 
-    const cleanedMonth = month[0].toUpperCase() + month.slice(1);
-    const maxLimit = maxLimitInput.value.trim();
-    const floatParsedLimit = parseFloat(maxLimit);
-    if (!validMonths.includes(cleanedMonth)) {
-        monthInput.setCustomValidity("Please enter a valid month of the year.");
-        monthInput.reportValidity();
-        return;
-    }
+  const cleanedMonth = month[0].toUpperCase() + month.slice(1);
+  const maxLimit = maxLimitInput.value.trim();
+  const floatParsedLimit = parseFloat(maxLimit);
+  if (!validMonths.includes(cleanedMonth)) {
+    monthInput.setCustomValidity("Please enter a valid month of the year.");
+    monthInput.reportValidity();
+    return;
+  }
 
-    const dupedMonth = monthExpensesData.some(i => i.month === cleanedMonth);
-    if (dupedMonth) {
-        monthInput.setCustomValidity("This month already existed.");
-        monthInput.reportValidity();
-        return;
-    }
+  const dupedMonth = monthExpensesData.some((i) => i.month === cleanedMonth);
+  if (dupedMonth) {
+    monthInput.setCustomValidity("This month already existed.");
+    monthInput.reportValidity();
+    return;
+  }
 
-    monthInput.setCustomValidity("");
-    if (floatParsedLimit <= 0 || isNaN(floatParsedLimit)) {
-        maxLimitInput.setCustomValidity("Please enter a valid number as the budget limit.");
-        maxLimitInput.reportValidity();
-        return;
-    }
-    maxLimitInput.setCustomValidity("");
+  monthInput.setCustomValidity("");
+  if (floatParsedLimit <= 0 || isNaN(floatParsedLimit)) {
+    maxLimitInput.setCustomValidity(
+      "Please enter a valid number as the budget limit.",
+    );
+    maxLimitInput.reportValidity();
+    return;
+  }
+  maxLimitInput.setCustomValidity("");
 
-    const toAdd = {
-        id: Date.now(),
-        month: cleanedMonth,
-        limit: floatParsedLimit,
-        expenses: []
-    }
+  const toAdd = {
+    id: Date.now(),
+    month: cleanedMonth,
+    limit: floatParsedLimit,
+    expenses: [],
+  };
 
-    monthExpensesData.push(toAdd);
-    saveData();
-    renderMonths();
-    monthInputForm.reset();
-    monthInputDialog.close();
+  monthExpensesData.push(toAdd);
+  saveData();
+  renderMonths();
+  monthInputForm.reset();
+  monthInputDialog.close();
 }
 
 function updateMainPanelStats() {
-    const month = monthExpensesData.find(m => m.id === currentMonthOpenedId);
+  const month = monthExpensesData.find((m) => m.id === currentMonthOpenedId);
 
-    if (month) {
-        mainPanelHeader.textContent = month.month;
-        const totalExpenses = month.expenses.length === 0 ? 0 : month.expenses.reduce((a, b) => a + b.amount, 0);
-        const percentage = (totalExpenses / month.limit) * 100;
-        if (percentage < 75) {
-            percentageBar.style.backgroundColor = "green";
-        } else if (percentage >= 75 && percentage < 100) {
-            percentageBar.style.backgroundColor = "yellow";
-        } else {
-            percentageBar.style.backgroundColor = "red";
-        }
-        percentageBar.style.width = percentage > 100 ? "100%" : `${percentage.toFixed(2)}%`;
-        budgetText.textContent = `$${totalExpenses}/$${month.limit}`;
+  if (month) {
+    mainPanelHeader.textContent = month.month;
+    const totalExpenses =
+      month.expenses.length === 0
+        ? 0
+        : month.expenses.reduce((a, b) => a + b.amount, 0);
+    const percentage = (totalExpenses / month.limit) * 100;
+    if (percentage < 75) {
+      percentageBar.style.backgroundColor = "green";
+    } else if (percentage >= 75 && percentage < 100) {
+      percentageBar.style.backgroundColor = "yellow";
+    } else {
+      percentageBar.style.backgroundColor = "red";
     }
-} 
+    percentageBar.style.width =
+      percentage > 100 ? "100%" : `${percentage.toFixed(2)}%`;
+    budgetText.textContent = `$${totalExpenses}/$${month.limit}`;
+  }
+}
 
 function addExpense() {
-    if (currentMonthOpenedId === null) return;
+  if (currentMonthOpenedId === null) return;
 
-    const currentMonth = monthExpensesData.find(m => m.id === currentMonthOpenedId);
+  const currentMonth = monthExpensesData.find(
+    (m) => m.id === currentMonthOpenedId,
+  );
 
-    const expenseInput = expInput.value.trim().toLowerCase();
+  const expenseInput = expInput.value.trim().toLowerCase();
 
-    if (!expenseInput) {
-        expInput.setCustomValidity("Please enter an expense.")
-        expInput.reportValidity();
-        return;
-    }
-    expInput.setCustomValidity("");
+  if (!expenseInput) {
+    expInput.setCustomValidity("Please enter an expense.");
+    expInput.reportValidity();
+    return;
+  }
+  expInput.setCustomValidity("");
 
-    const cleanedExp = expenseInput[0].toUpperCase() + expenseInput.slice(1);
-    const amount = amountInput.value.trim();
-    const parsedAmount = parseFloat(amount);
+  const cleanedExp = expenseInput[0].toUpperCase() + expenseInput.slice(1);
+  const amount = amountInput.value.trim();
+  const parsedAmount = parseFloat(amount);
 
-    if (parsedAmount <= 0 || isNaN(parsedAmount)) {
-        amountInput.setCustomValidity("Please enter a valid amount.");
-        amountInput.reportValidity();
-        return;
-    }
-    amountInput.setCustomValidity("");
+  if (parsedAmount <= 0 || isNaN(parsedAmount)) {
+    amountInput.setCustomValidity("Please enter a valid amount.");
+    amountInput.reportValidity();
+    return;
+  }
+  amountInput.setCustomValidity("");
 
-    const newExpense = {
-        id: Date.now(),
-        expense: cleanedExp,
-        amount: parsedAmount
-    }
+  const newExpense = {
+    id: Date.now(),
+    expense: cleanedExp,
+    amount: parsedAmount,
+  };
 
-    currentMonth.expenses.push(newExpense);
-    saveData();
-    expenseForm.reset();
-    updateMainPanelStats();
-    renderExpenses();
+  currentMonth.expenses.push(newExpense);
+  saveData();
+  expenseForm.reset();
+  updateMainPanelStats();
+  renderExpenses();
 }
 
 function renderExpenses() {
-    const month = monthExpensesData.find(e => e.id === currentMonthOpenedId);
-    expenseList.innerHTML = "";
+  const month = monthExpensesData.find((e) => e.id === currentMonthOpenedId);
+  expenseList.innerHTML = "";
 
-    month.expenses.forEach((e) => {
-        const li = document.createElement("li");
-        li.className = "expense-item";
-        li.id = e.id;
+  month.expenses.forEach((e) => {
+    const li = document.createElement("li");
+    li.className = "expense-item";
+    li.id = e.id;
 
-        const spanExp = document.createElement("span");
-        spanExp.className = "expense-name";
-        spanExp.textContent = e.expense;
+    const spanExp = document.createElement("span");
+    spanExp.className = "expense-name";
+    spanExp.textContent = e.expense;
 
-        const spanAmount = document.createElement("span");
-        spanAmount.className = "expense-amount";
-        spanAmount.textContent = `$${e.amount}`;
+    const spanAmount = document.createElement("span");
+    spanAmount.className = "expense-amount";
+    spanAmount.textContent = `$${e.amount}`;
 
-        const delBtn = document.createElement("button");
-        delBtn.ariaLabel = "Delete expense";
-        delBtn.className = "delete-btn";
+    const delBtn = document.createElement("button");
+    delBtn.ariaLabel = "Delete expense";
+    delBtn.className = "delete-btn";
 
-        delBtn.addEventListener("click", () => {
-            deleteExpense(e.id);
-        })
+    delBtn.addEventListener("click", () => {
+      deleteExpense(e.id);
+    });
 
-        const i = document.createElement("i");
-        i.className = "fa-solid fa-trash";
+    const i = document.createElement("i");
+    i.className = "fa-solid fa-trash";
 
-        const editBtn = document.createElement("button");
-        editBtn.ariaLabel = "Edit expense";
-        editBtn.className = "edit-exp-btn";
+    const editBtn = document.createElement("button");
+    editBtn.ariaLabel = "Edit expense";
+    editBtn.className = "edit-exp-btn";
 
-        editBtn.addEventListener("click", () => {
-            openEditExpense(e.id);
-        })
+    editBtn.addEventListener("click", () => {
+      openEditExpense(e.id);
+    });
 
-        const i2 = document.createElement("i");
-        i2.className = "fa-regular fa-pen-to-square";
+    const i2 = document.createElement("i");
+    i2.className = "fa-regular fa-pen-to-square";
 
-        delBtn.appendChild(i);
-        editBtn.appendChild(i2);
+    delBtn.appendChild(i);
+    editBtn.appendChild(i2);
 
-        li.appendChild(spanExp);
-        li.appendChild(spanAmount);
-        li.appendChild(delBtn);
-        li.appendChild(editBtn);
+    li.appendChild(spanExp);
+    li.appendChild(spanAmount);
+    li.appendChild(delBtn);
+    li.appendChild(editBtn);
 
-        expenseList.appendChild(li);
-    })
+    expenseList.appendChild(li);
+  });
 }
 
 function openEditExpense(id) {
-    const month = monthExpensesData.find(m => m.id === currentMonthOpenedId);
-    const expe = month.expenses.find(e => e.id === id);
+  const month = monthExpensesData.find((m) => m.id === currentMonthOpenedId);
+  const expe = month.expenses.find((e) => e.id === id);
 
-    currentExpenseEditedId = id
+  currentExpenseEditedId = id;
 
-    newExp.value = expe.expense;
-    newAmount.value = expe.amount;
+  newExp.value = expe.expense;
+  newAmount.value = expe.amount;
 
-    editDialog.showModal();
+  editDialog.showModal();
 }
 
 function editExpense() {
-    const month = monthExpensesData.find(m => m.id === currentMonthOpenedId);
-    const expe = month.expenses.find(e => e.id === currentExpenseEditedId);
+  const month = monthExpensesData.find((m) => m.id === currentMonthOpenedId);
+  const expe = month.expenses.find((e) => e.id === currentExpenseEditedId);
 
-    const newExpInput = newExp.value.trim();
-    if (!newExpInput) {
-        newExp.setCustomValidity("Please enter a valid expense.")
-        newExp.reportValidity();
-        return;
-    }
-    newExp.setCustomValidity("");
-    const cleanedNewExp = newExpInput[0].toUpperCase() + newExpInput.slice(1).toLowerCase();
-    const parsedNewAmount = parseFloat(newAmount.value.trim());
+  const newExpInput = newExp.value.trim();
+  if (!newExpInput) {
+    newExp.setCustomValidity("Please enter a valid expense.");
+    newExp.reportValidity();
+    return;
+  }
+  newExp.setCustomValidity("");
+  const cleanedNewExp =
+    newExpInput[0].toUpperCase() + newExpInput.slice(1).toLowerCase();
+  const parsedNewAmount = parseFloat(newAmount.value.trim());
 
+  if (parsedNewAmount <= 0 || isNaN(parsedNewAmount)) {
+    newAmount.setCustomValidity("Please enter a valid amount.");
+    newAmount.reportValidity();
+    return;
+  }
+  newAmount.setCustomValidity("");
 
-    if (parsedNewAmount <= 0 || isNaN(parsedNewAmount)) {
-        newAmount.setCustomValidity("Please enter a valid amount.");
-        newAmount.reportValidity();
-        return;
-    }
-    newAmount.setCustomValidity("");
+  expe.expense = cleanedNewExp;
+  expe.amount = parsedNewAmount;
 
-    expe.expense = cleanedNewExp;
-    expe.amount = parsedNewAmount;
-
-    saveData();
-    renderExpenses();
-    updateMainPanelStats();
-    editForm.reset();
-    editDialog.close();
-    currentExpenseEditedId = null;
+  saveData();
+  renderExpenses();
+  updateMainPanelStats();
+  editForm.reset();
+  editDialog.close();
+  currentExpenseEditedId = null;
 }
 
 function deleteExpense(id) {
-    const month = monthExpensesData.find(m => m.id === currentMonthOpenedId);
+  const month = monthExpensesData.find((m) => m.id === currentMonthOpenedId);
 
-    const newExpenses = month.expenses.filter(e => e.id !== id);
-    month.expenses = newExpenses;
-    saveData();
-    updateMainPanelStats();
-    renderExpenses();
+  const newExpenses = month.expenses.filter((e) => e.id !== id);
+  month.expenses = newExpenses;
+  saveData();
+  updateMainPanelStats();
+  renderExpenses();
 }
 
 function deleteMonth(id) {
-    if (!confirm("Are you sure you wanna delete this month including every expenses in it ?")) {
-        return;
-    }
+  if (
+    !confirm(
+      "Are you sure you wanna delete this month including every expenses in it ?",
+    )
+  ) {
+    return;
+  }
 
-    monthExpensesData = monthExpensesData.filter(m => m.id !== id);
-    saveData();
-    renderMonths();
+  monthExpensesData = monthExpensesData.filter((m) => m.id !== id);
+  saveData();
+  renderMonths();
 
-    if (currentMonthOpenedId === id) {
-        currentMonthOpenedId = null;
-        mainPanelHeader.textContent = "";
-        percentageBar.style.width = "0%";
-        budgetText.textContent = "";
-        expenseList.innerHTML = "";
-    }
+  if (currentMonthOpenedId === id) {
+    currentMonthOpenedId = null;
+    mainPanelHeader.textContent = "";
+    percentageBar.style.width = "0%";
+    budgetText.textContent = "";
+    expenseList.innerHTML = "";
+  }
 }
 
 monthInputForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addMonth();
-})
+  e.preventDefault();
+  addMonth();
+});
 
 expenseForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    addExpense();
-})
+  e.preventDefault();
+  addExpense();
+});
 
 editForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    editExpense();
-})
+  e.preventDefault();
+  editExpense();
+});
 
 openMonthInputBtn.addEventListener("click", () => {
-    monthInputDialog.showModal();
-})
+  monthInputDialog.showModal();
+});
 
 cancelMonthInput.addEventListener("click", () => {
-    monthInputDialog.close();
-})
+  monthInputDialog.close();
+});
 
 cancelEditBtn.addEventListener("click", (e) => {
-    newExp.setCustomValidity("");
-    newAmount.setCustomValidity("");
-    editDialog.close();
-    currentExpenseEditedId = null;
-})
+  newExp.setCustomValidity("");
+  newAmount.setCustomValidity("");
+  editDialog.close();
+  currentExpenseEditedId = null;
+});
 
 monthInput.addEventListener("input", () => monthInput.setCustomValidity(""));
-maxLimitInput.addEventListener("input", () => maxLimitInput.setCustomValidity(""));
+maxLimitInput.addEventListener("input", () =>
+  maxLimitInput.setCustomValidity(""),
+);
 expInput.addEventListener("input", () => expInput.setCustomValidity(""));
 amountInput.addEventListener("input", () => amountInput.setCustomValidity(""));
 newExp.addEventListener("input", () => newExp.setCustomValidity(""));
